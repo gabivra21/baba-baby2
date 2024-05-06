919e95426276f388108465d064faa9c898199817
Gabi <vieira.rgabi@gmail.com>
2024-05-06 10:58:15 -0300

    colocando comentários

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -8,4 +8,5 @@ $baseDir = dirname($vendorDir);
 return array(
     'Sts\\' => array($baseDir . '/app/sts'),
     'Core\\' => array($baseDir . '/core'),
+    /*diretorio core terá a classe responsável por indicar qual página o usuário esta tentando acessar, qual view será apresentada para o usuário */
 );
