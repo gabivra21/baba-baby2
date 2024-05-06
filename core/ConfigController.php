eb298a6cd38ebd8d21240cb516347bcdfe443f3a
nicole fatuch <nfatuch@gmail.com>
2024-05-06 11:22:42 -0300

    core feito

--- core/ConfigController.php
+++ core/ConfigController.php
@@ -5,6 +5,7 @@ namespace Core;
 class ConfigController
 {
     private string $url;
+    private array $urlArray;
     private string $urlController;
     private string $urlMetodo;
 
@@ -12,7 +13,18 @@ class ConfigController
     {
         if(!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT))){
             $this->url = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
-            var_dump($this->url);
+            //var_dump($this->url);
+            $this->urlArray = explode("/", $this->url);
+            //var_dump($this->urlArray);
+            
+
+            if((isset($this->urlArray[0])) and (isset($this->urlArray[1]))){
+                $this->urlController = $this->urlArray[0];
+            $this->urlMetodo = $this->urlArray[1];
+            }else{
+                $this->urlController = "erro";
+                $this->urlMetodo = "index";
+            }
         }else{
             $this->urlController = "home";
             $this->urlMetodo = "index";
