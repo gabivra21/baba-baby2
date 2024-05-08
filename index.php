7608de8fed240d75aaca0d4aede2a8927807b04e
nicole fatuch <nfatuch@gmail.com>
2024-05-08 18:37:29 -0300

    login iniciado

--- index.php
+++ index.php
@@ -5,10 +5,22 @@
         <title>BabaBaby</title>
     </head>
     <body>
+        
+        <h1>Login</h1>
+
         <?php
-            require './vendor/autoload.php';
-            $url = new Core\ConfigController();
-            $url->loadPage();
+            $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+            var_dump($dados)
         ?>
+
+        <form method="POST" action="">
+            <label>Email:</label>
+            <input type="text" name="usuario" placeholder="Digite o email">
+            <br><br>
+            <label>Senha:</label>
+            <input type="password" name="senha_usuario" placeholder="Digite a senha">
+            <br><br>
+            <input type="submit" value="Acessar" name="SendLogin">
+        </form>
     </body>
 </html>
