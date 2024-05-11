34d2063ca6372609093ea5fd8794913f05d2b274
Gabi <vieira.rgabi@gmail.com>
2024-05-11 19:58:47 -0300

    alterando tudo

--- sprint1/excluirCRIA.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require 'configu.php';
-    /*delete crianca*/
-    $id = filter_input(INPUT_GET, 'idCrianca');
-    $sql = $pdo->prepare("DELETE FROM crianca WHERE idCrianca = :idCrianca");
-    $sql->bindValue(':idCrianca', $id);
-    $sql->execute();
-    header("Location: selectCRIA.php");
-?>
