964572880943e4e6f768931fbca1c8ac4d4c7e08
Felipe Brugnera <165804740+lipebrug@users.noreply.github.com>
2024-05-11 20:57:22 -0300

    Add files via upload

---
+++ sprint2/pais/menuPAIS.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Baba Baby</title>
+</head>
+<body>
+    <form action="search.php" method="GET">
+        <input type="text" name="search_query" placeholder="Pesquise a babá por nome ou cidade">
+        <input type="submit" value="Pesquisar">
+    </form>
+
+</body>
+</html>
