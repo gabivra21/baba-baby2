263234da999ed5ee2ae8f462a719c4f9440521cb
Ana Paula Militao <anapaulammilitao@hotmail.com>
2024-05-13 00:11:42 -0300

    cadastro de usuário funcionando, faltando apenas registro no banco de pai e baba

--- src/cadastro/js/cadastrar.js
+++ src/cadastro/js/cadastrar.js
@@ -1,3 +1,4 @@
+
 //COMPONENTES
 
 const btnSouPai = document.getElementById("btn-pai");
@@ -8,6 +9,7 @@ const btnNao = document.getElementById("btn-nao");
 const modalSouPai = document.getElementById("modal-pai");
 const modalSouBaba = document.getElementById("modal-baba");
 const modalCancelar = document.getElementById("modal-cancelar");
+const btnModais = document.getElementsByClassName("modalForm");
 
 //FUNÇÕES
 
@@ -33,3 +35,28 @@ btnSim.onclick = () => {
 btnNao.onclick = () => {
     modalCancelar.close();
 }
+
+$(document).ready(() => {
+    for (var i = 0; i < btnModais.length; i++) {
+        btnModais[i].addEventListener('click', () => {
+            var form = new FormData($("#formUsuario")[0]);
+            $.ajax({
+                url: "backend/cadastrarUsuario.php",
+                type: "post",
+                dataType: "json",
+                cache: false,
+                processData: false,
+                contentType: false,
+                data: form,
+                timeout: 8000,
+                success: (result) => {
+                   console.log(form.values())
+                }
+            });
+        });
+    }
+});
+
+
+
+
