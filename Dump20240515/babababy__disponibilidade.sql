497b4953f7f86eb60b7d72bc036f6f785f9f121b
Gabi <vieira.rgabi@gmail.com>
2024-05-15 20:38:18 -0300

    tentando consertar o cookie

---
+++ Dump20240515/babababy__disponibilidade.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: babababy_
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `disponibilidade`
+--
+
+DROP TABLE IF EXISTS `disponibilidade`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `disponibilidade` (
+  `idDisponibilidade` int NOT NULL AUTO_INCREMENT,
+  `fk_idDia` int NOT NULL,
+  `fk_idTurno` int NOT NULL,
+  `fk_idBaba` int NOT NULL,
+  PRIMARY KEY (`idDisponibilidade`),
+  KEY `fk_idDia_idx` (`fk_idDia`),
+  KEY `fk_idTurno_idx` (`fk_idTurno`),
+  KEY `fk_idBaba` (`fk_idBaba`),
+  CONSTRAINT `fk_idBaba` FOREIGN KEY (`fk_idBaba`) REFERENCES `baba` (`idBaba`),
+  CONSTRAINT `fk_idDia` FOREIGN KEY (`fk_idDia`) REFERENCES `dia` (`idDia`),
+  CONSTRAINT `fk_idTurno` FOREIGN KEY (`fk_idTurno`) REFERENCES `turno` (`idTurno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `disponibilidade`
+--
+
+LOCK TABLES `disponibilidade` WRITE;
+/*!40000 ALTER TABLE `disponibilidade` DISABLE KEYS */;
+/*!40000 ALTER TABLE `disponibilidade` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-15 16:17:13
--- src/cadastro/backend/cadastroPai.php
+++ src/cadastro/backend/cadastroPai.php
@@ -2,7 +2,17 @@
 
 require '../../database/configu.php';
 
-$idUsuario = $_COOKIE['idUsuario'];
+$value = 'idUsuario';
+setcookie("CookieTeste", $value);
+setcookie("CookieTeste", $value, time() +3600 );
+setcookie("CookieTeste", $value, time() +3600,"/baba-baby2/src/cadastro/", "localhost", 1);
+
+print_r($_COOKIE);
+
+
+
+
+$idUsuario = $_COOKIE['CookieTeste'];
 $qtdecrianca = filter_input(INPUT_POST, 'qtdeCrianca');
 $descricao = filter_input(INPUT_POST, 'descricao');
 
@@ -14,3 +24,5 @@ $cadastroPaiSQL->bindValue(':idUsuario', $idUsuario);
 $cadastroPaiSQL->execute();
 
 header("Location: ../../usuario/select.php");
+
+?>
