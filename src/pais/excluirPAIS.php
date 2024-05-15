12da9e521d9348891c2dcced64b7d7396ac26179
Ana Paula Militao <anapaulammilitao@hotmail.com>
2024-05-15 12:57:03 -0300

    nova remoção de pastas

--- src/pais/excluirPAIS.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require 'configu.php';
-    /*delete PAIS*/
-    $id = filter_input(INPUT_GET, 'idPais');
-    $sql = $pdo->prepare("DELETE FROM pais WHERE idPais = :idPais");
-    $sql->bindValue(':idPais', $id);
-    $sql->execute();
-    header("Location: selectPAIS.php");
-?>
