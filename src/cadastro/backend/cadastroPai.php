0a0b246042ca5069bf6840afd0968de9e6b2f49c
Gabi <vieira.rgabi@gmail.com>
2024-05-16 11:29:18 -0300

    cuazi

--- src/cadastro/backend/cadastroPai.php
+++ src/cadastro/backend/cadastroPai.php
@@ -2,10 +2,11 @@
 
 require '../../database/configu.php';
 
-$value = 'idUsuario';
+$idUsuario = $pdo->lastInsertId();
+$value = $idUsuario;
 setcookie("CookieTeste", $value);
 setcookie("CookieTeste", $value, time() +3600 );
-setcookie("CookieTeste", $value, time() +3600,"/baba-baby2/src/cadastro/", "localhost", 1);
+//setcookie("CookieTeste", $value, time() +3600,"/baba-baby2/src/cadastro/", "localhost", 1);
 
 print_r($_COOKIE);
 
@@ -13,11 +14,11 @@ print_r($_COOKIE);
 
 
 $idUsuario = $_COOKIE['CookieTeste'];
-$qtdecrianca = filter_input(INPUT_POST, 'qtdeCrianca');
+$qtdeCrianca = filter_input(INPUT_POST, 'qtdeCrianca');
 $descricao = filter_input(INPUT_POST, 'descricao');
 
 $cadastroPaiSQL = $pdo->prepare("INSERT INTO pais (qtdeCrianca, descricao, pk_idUsuario) VALUES (:qtdeCrianca, :descricao, :idUsuario);");
-$cadastroPaiSQL->bindValue(':qtdeCrianca', $qtdecrianca);
+$cadastroPaiSQL->bindValue(':qtdeCrianca', $qtdeCrianca);
 $cadastroPaiSQL->bindValue(':descricao', $descricao);
 $cadastroPaiSQL->bindValue(':idUsuario', $idUsuario);
 
